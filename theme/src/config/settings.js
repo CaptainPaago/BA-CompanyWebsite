63e5db812c5bc4e379da7334d783a4789ccf9f39
Dele Paago <delebliss42@gmail.com>
2023-06-07 15:12:28 +0000

    Some changes in the package.json

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -4,5 +4,5 @@ export default {
 	url: `https://blisseladvisory.netlify.app`, // No trailing slash!
 	name: `Blissel Advisory Inc`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
